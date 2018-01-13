1feea30ec6a08a1898ae7b47a06b3c69017faa8d
MiloszSzatkowski <milosz.dawid.szatkowski@gmail.com>
2018-01-13 20:33:04 +0100

    Started working on update on block function

--- Z__SCRIPTS/Deck.cs
+++ Z__SCRIPTS/Deck.cs
@@ -23,6 +23,8 @@ public class Deck : MonoBehaviour {
 
 	public int temp;
 	public Debbug_Logger_Script Deb;
+	public Draggable _Draggable;
+	public Slot_Watcher _Slot_Watcher;
 
 	void Start () {
 		//creating random list of indexes
@@ -57,6 +59,74 @@ public class Deck : MonoBehaviour {
 				}
 				// end of Start
 				Deb.Debug_Logger("Game has started. Good Luck.");
+				//Update started
+				InvokeRepeating("blockEvent", 0, 1);
+		}
+
+		//UPDATE
+
+
+		void blockEvent(){
+			if(areAllCardsOnTheTableBlocked()){
+				Deb.Debug_Logger("Block Event happening.");
+				// _Draggable.
+			}
+		}
+
+		public bool areAllCardsOnTheTableBlocked(){
+			if (allPlacesAreOccupated()
+			&& areThereCardsOnPilesInTheMiddle())
+		  {
+				return true;
+			} else if (
+				 player1_cards.Count == 0
+			&& player2_cards.Count == 0
+			&& areThereCardsOnPilesInTheMiddle()){
+				return true;
+			} else if (
+			player1_cards.Count == 0
+			&&	_Slot_Watcher.slot5==true
+			&&	_Slot_Watcher.slot6==true
+			&&	_Slot_Watcher.slot7==true
+			&&	_Slot_Watcher.slot8==true
+			&& areThereCardsOnPilesInTheMiddle()
+			){
+				return true;
+			} else if (
+			player2_cards.Count == 0
+			&&	_Slot_Watcher.slot1==true
+			&&	_Slot_Watcher.slot2==true
+			&&	_Slot_Watcher.slot3==true
+			&&	_Slot_Watcher.slot4==true
+			&& areThereCardsOnPilesInTheMiddle()
+			){
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public bool areThereCardsOnPilesInTheMiddle(){
+			if (stosLewa.Count  > 0
+			&&  stosPrawa.Count > 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public bool allPlacesAreOccupated(){
+			if (
+					_Slot_Watcher.slot1==true
+			&&	_Slot_Watcher.slot2==true
+			&&	_Slot_Watcher.slot3==true
+			&&	_Slot_Watcher.slot4==true
+			&&	_Slot_Watcher.slot5==true
+			&&	_Slot_Watcher.slot6==true
+			&&	_Slot_Watcher.slot7==true
+			&&	_Slot_Watcher.slot8==true
+			){ return true;  } else
+			 { return false; }
 		}
 
 	// end of MonoBehaviour
