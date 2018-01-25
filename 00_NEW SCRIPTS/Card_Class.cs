6558cc11e78486953fb0cb9cee025ba90e648645
MiloszSzatkowski <milosz.dawid.szatkowski@gmail.com>
2018-01-25 23:43:06 +0100

    Building new game from scratch

---
+++ 00_NEW SCRIPTS/Card_Class.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Card_Class : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler {
+
+	//suits: of Hearts, Diamonds, Clubs, Spades
+	public List<string> suits;
+	public string card_suit;
+
+	//values : 2,3,4,5,6,7,8,9,10,Jack,Queen,King,Ace,Joker
+	public List<string> values;
+	public string card_value;
+
+	public Vector2 dragOffset = new Vector2(0f,0f);
+	public Vector2 startPosition = new Vector2(0f,0f);
+
+	public void OnBeginDrag (PointerEventData eventData){
+			dragOffset = eventData.position - (Vector2)this.transform.position;
+	}
+
+	public void OnDrag (PointerEventData eventData){
+			this.transform.position = eventData.position - dragOffset;
+	}
+
+	public void OnEndDrag (PointerEventData eventData) {
+
+	}
+
+}
---
+++ 00_NEW SCRIPTS/Initialize.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize : MonoBehaviour {
+
+	public GameObject Card;
+	public GameObject CardInstance;
+
+	// Use this for initialization
+	void Start () {
+		int suits_counter = 0;
+		int values_counter = 0;
+		for (int i = 0; i < 13*4; i++) {
+			//counter
+			CardInstance = Instantiate(Card, Vector2.zero , Quaternion.identity);
+			Debug.Log("Cards intantiated");
+
+			Card_Class _Card_Class = CardInstance.GetComponent<Card_Class>();
+
+			_Card_Class.suits = new List<string> {"Hearts", "Diamonds", "Clubs", "Spades"} ;
+			_Card_Class.values = new List<string> {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace","Joker"} ;
+
+			if (suits_counter == _Card_Class.suits.Count) {
+				suits_counter = 0; }
+			
+				_Card_Class.card_value = (string) _Card_Class.suits[suits_counter];
+				Debug.Log(suits_counter);
+
+			if (values_counter == _Card_Class.values.Count) {
+				values_counter = 0; }
+
+				_Card_Class.card_suit  = (string) _Card_Class.values[values_counter];
+				Debug.Log(values_counter);
+
+			suits_counter=suits_counter+1;
+			values_counter=values_counter+1;
+		}
+	}
+
+}
